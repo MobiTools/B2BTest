7ec5900bb33edd97fb2ad9e485de0da1f1b7b342
Your Git Username <radu.p1995@yahoo.com>
2023-10-26 20:40:23 +0300

    26.10.2023

--- next.config.js
+++ next.config.js
@@ -1,18 +1,9 @@
-// Enable this code below for Server Side Rendering/Translation (SSR)
-
 const withImages = require("next-images");
 const ESLintPlugin = require("eslint-webpack-plugin");
 
 module.exports = withImages({
-  // Enable this code below for Server Side Rendering/Translation (SSR)
-
   trailingSlash: true,
-  images: {
-    disableStaticImages: true,
-  },
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
   publicRuntimeConfig: {
@@ -22,13 +13,12 @@ module.exports = withImages({
         : "none",
   },
   webpack: (config, options) => {
-    true,
-      config.plugins
-        .push
-        //      new ESLintPlugin({
-        //        exclude: ['node_modules']
-        //      })
-        ();
+    config.plugins
+      .push
+      // new ESLintPlugin({
+      //   exclude: ['node_modules']
+      // })
+      ();
     return config;
   },
 });
