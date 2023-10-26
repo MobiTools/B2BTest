0c789a5e0b05e5fa75097354bc7a693d101802fa
Your Git Username <radu.p1995@yahoo.com>
2023-10-26 21:11:17 +0300

    25.10.2023

--- next.config.js
+++ next.config.js
@@ -24,24 +24,4 @@ module.exports = withImages({
       ();
     return config;
   },
-
-  // Adăugați configurația pentru a exclude paginile cu getServerSideProps de la exportul static.
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    if (dev) {
-      return defaultPathMap;
-    }
-
-    const filteredPathMap = { ...defaultPathMap };
-
-    // Excludem paginile cu getServerSideProps de aici
-    delete filteredPathMap["/"];
-    delete filteredPathMap["/home"];
-    delete filteredPathMap["/services"];
-    delete filteredPathMap["/news"];
-
-    return filteredPathMap;
-  },
 });
