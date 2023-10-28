7fa8c42ebd11dab937727c8b23c44f09482d8513
Your Git Username <radu.p1995@yahoo.com>
2023-10-28 14:42:14 +0300

    Another

---
+++ pages/_document.jsx
@@ -0,0 +1,95 @@
+import React from "react";
+import Document, { Html, Main, NextScript } from "next/document";
+import createEmotionServer from "@emotion/server/create-instance";
+import { getInitColorSchemeScript } from "@mui/material/styles";
+import createEmotionCache from "../theme/createEmotionCache";
+import HeadComponent from "~/components/head";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <HeadComponent />
+        <body>
+          <div
+            id="preloader"
+            style={{
+              position: "fixed",
+              zIndex: 10000,
+              background: "black",
+              width: "100%",
+              height: "100%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <img src="/loadGif.gif" alt="loading" />
+          </div>
+
+          {getInitColorSchemeScript()}
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+MyDocument.getInitialProps = async (ctx) => {
+  // Resolution order
+  //
+  // On the server:
+  // 1. app.getInitialProps
+  // 2. page.getInitialProps
+  // 3. document.getInitialProps
+  // 4. app.render
+  // 5. page.render
+  // 6. document.render
+  //
+  // On the server with error:
+  // 1. document.getInitialProps
+  // 2. app.render
+  // 3. page.render
+  // 4. document.render
+  //
+  // On the client
+  // 1. app.getInitialProps
+  // 2. page.getInitialProps
+  // 3. app.render
+  // 4. page.render
+
+  // Render app and page and get the context of the page with collected side effects.
+  const originalRenderPage = ctx.renderPage;
+
+  const cache = createEmotionCache();
+  const { extractCriticalToChunks } = createEmotionServer(cache);
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) =>
+        function EnhanceApp(props) {
+          // eslint-disable-line
+          return <App emotionCache={cache} {...props} />;
+        },
+    });
+
+  const initialProps = await Document.getInitialProps(ctx);
+  const emotionStyles = extractCriticalToChunks(initialProps.html);
+  const emotionStyleTags = emotionStyles.styles.map((style) => (
+    <style
+      data-emotion={`${style.key} ${style.ids.join(" ")}`}
+      key={style.key}
+      // eslint-disable-next-line react/no-danger
+      dangerouslySetInnerHTML={{ __html: style.css }}
+    />
+  ));
+
+  return {
+    ...initialProps,
+    emotionStyleTags,
+    namespacesRequired: ["common", "starter-landing"],
+  };
+};
+
+export default MyDocument;
